c5e63bb18c86dad9cb3bff7a97d5a91e92154757
kuper3 <qwerty_144@e-mail.ua>
2012-09-10 22:42:48 +0300

    add coffeescript suport(very simple)

--- project/Build.scala
+++ project/Build.scala
@@ -15,6 +15,7 @@ object ApplicationBuild extends Build {
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
       // Add your own project settings here      
+        coffeescriptOptions := Seq("bare")
     )
 
 }
